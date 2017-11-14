bfc0b7f7023a1c8f646daff21ad8f294521e3199
Tamir Moshiashvili <norblet82@gmail.com>
2017-11-15 01:09:09 +0200

    # HMM task 1 - completed the code but yet not checked

---
+++ code/MLETrain.py
@@ -0,0 +1,54 @@
+import sys
+from collections import Counter
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+
+    # read and collect data on the text
+    train_file = open(args[0], 'r')
+    lines = train_file.read().splitlines()
+    e_mle_counter = Counter()
+    q_mle_counter = Counter()
+    for line in lines:
+        words_iter = iter(line.split(' '))
+
+        # first 2 items - e.mle update
+        prev_2 = next(words_iter).rsplit('/', 1)
+        tag2 = prev_2[1]
+        e_mle_counter.update((prev_2[0], tag2))
+
+        prev_1 = next(words_iter).rsplit('/', 1)
+        tag1 = prev_1[1]
+        e_mle_counter.update((prev_1[0], tag1))
+
+        # first 2 items - q.mle update
+        q_mle_counter.update([tag2, tag1, (tag2, tag1)])
+
+        for item in words_iter:
+            word, tag = item.rsplit('/', 1)
+
+            # e.mle update
+            e_mle_counter.update((word, tag))
+
+            # q.mle update
+            q_mle_counter.update([tag, (tag1, tag), (tag2, tag1, tag)])
+
+            # update last 2 items
+            tag2 = tag1
+            tag1 = tag
+
+    # write to e.mle
+    e_mle_file = open(args[1], 'w')
+    for (word, tag), n in e_mle_counter.items():
+        e_mle_file.write(word + ' ' + tag + '\t' + str(n))
+    e_mle_file.close()
+
+    # write to q.mle
+    q_mle_file = open(args[2], 'w')
+    for tup, n in q_mle_counter.items():
+        for tag in list(tup):
+            q_mle_file.write(tag + ' ')
+        q_mle_file.write('\t' + str(n))
+    q_mle_file.close()
+
+    train_file.close()
