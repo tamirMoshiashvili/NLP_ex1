928ee1377b83f52c7a89d951cbcab7397cc858d5
Tamir <Tamir@DESKTOP-8R67EMV>
2017-11-17 01:18:04 +0200

    # MLETrain improvements

--- code/MLETrain.py
+++ code/MLETrain.py
@@ -1,5 +1,6 @@
 import sys
 from collections import Counter
+from time import time
 
 
 def write_to_file_mle(counter, filename):
@@ -15,32 +16,43 @@ def write_to_file_mle(counter, filename):
     mle_file.close()
 
 
+def add_to_counter(counter, keys):
+    for key in keys:
+        if key in counter:
+            counter[key] += 1
+        else:
+            counter[key] = 1
+
+
 if __name__ == '__main__':
     # read and collect data on the text
-    train_file = open(sys.argv[1], 'r')
-    lines = train_file.read().splitlines()
-    train_file.close()
-    q_mle_counter = Counter()
-    e_mle_counter = Counter()
+    t = time()
 
-    for line in lines:
+    train_file = open(sys.argv[1], 'r')
+    q_mle_counter = dict()
+    e_mle_counter = dict()
+
+    for line in train_file.readlines():
         words_iter = iter(line.split(' '))
 
         # first 2 items - e.mle update
         tag2 = tag1 = '_START_'
-        q_mle_counter.update([(tag2,), (tag1,), (tag2, tag1)])
+        add_to_counter(q_mle_counter, [(tag2,), (tag1,), (tag2, tag1)])
 
         for item in words_iter:
             word, tag = item.rsplit('/', 1)
 
             # update counters
-            e_mle_counter.update([(word, tag)])
-            q_mle_counter.update([(tag,), (tag1, tag), (tag2, tag1, tag)])
+            add_to_counter(e_mle_counter, [(word, tag)])
+            add_to_counter(q_mle_counter, [(tag,), (tag1, tag), (tag2, tag1, tag)])
 
             # update last 2 tags
             tag2 = tag1
             tag1 = tag
+    train_file.close()
 
     # write to mle-files
     write_to_file_mle(q_mle_counter, sys.argv[2])
     write_to_file_mle(e_mle_counter, sys.argv[3])
+
+    print time() - t
