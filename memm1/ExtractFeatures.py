d2a8157cfc6ca382eabcfe7f9e697bc348a88c4e
Tamir <Tamir@DESKTOP-8R67EMV>
2017-11-20 01:23:57 +0200

    # implemented Features Extraction, yet did not test it

--- memm1/ExtractFeatures.py
+++ memm1/ExtractFeatures.py
@@ -1,6 +1,8 @@
 import sys
 from StringIO import StringIO
 
+START = '_START_'
+
 
 def read_file(filename):
     """
@@ -10,12 +12,12 @@ def read_file(filename):
     file_lines = f.read().splitlines()
     f.close()
 
-    for i, line in file_lines:
+    for i, line in enumerate(file_lines):
         line = line.split(' ')
         file_lines[i] = line
         for j, pair in enumerate(line):
             word, tag = pair.rsplit('/', 1)
-            file_lines[i][j] = (word, tag)
+            file_lines[i][j] = (word, tag)  # TODO: need to check the alert - says that it is a string
     return file_lines
 
 
@@ -34,12 +36,73 @@ def get_words_counter(lines):
     return counter
 
 
-def is_rare(counter, word):
+def fill_with_regular_features(features_dict, line, i):
+    """
+    :param features_dict: features dictionary.
+    :param line: list of (word, tag) tuples.
+    :param i: index of the current tuple in line.
+    """
+    if i > 0:
+        prev_word, prev_tag = line[i - 1]
+        features_dict['prev_word'] = prev_word
+        features_dict['prev_tag'] = prev_tag
+    else:
+        features_dict['prev_tag'] = START
+    if i > 1:
+        prev_prev_word, prev_prev_tag = line[i - 2]
+        features_dict['prev_prev_word'] = prev_prev_word
+        features_dict['prev_2_tags'] = prev_prev_tag + '/' + line[i - 1][1]
+    else:
+        features_dict['prev_2_tags'] = START + '/' + START
+
+    n = len(line)
+    if i < n - 1:
+        features_dict['next_word'] = line[i + 1][0]
+    if i < n - 2:
+        features_dict['next_next_word'] = line[i + 2][0]
+
+
+def is_rare(counter, w):
     """
     Check if the given word is rare, according to the MEMM-paper.
     :return: boolean.
     """
-    return counter[word] < 5
+    return counter[w] < 5
+
+
+def fill_with_rareness_features(features_dict, counter, w):
+    if is_rare(counter, w):
+        features_dict['contains_hyphen'] = '-' in w
+        features_dict['contains_number'] = any(char.isdigit() for char in w)
+        features_dict['contains_uppercase'] = any(char == char.upper() for char in w)
+
+        # prefixes and suffixes
+        n = len(w)
+        for j in range(n):
+            if n > j:
+                features_dict['prefix_' + str(j + 1)] = w[:j + 1]
+                features_dict['suffix_' + str(j + 1)] = w[n - j - 1:]
+    else:
+        features_dict['form'] = w
+
+
+def get_features_str(features_dict):
+    """
+    :param features_dict: features dictionary.
+    :return: string that describe the features-dictionary,
+             format - 'key0=val0 key1=val1 ... keyn=valn<end_line>'
+    """
+    feat_str = StringIO()
+    features_dict_iter_items = features_dict.iteritems()
+
+    # first element
+    key0, val0 = next(features_dict_iter_items)
+    feat_str.write(key0 + '=' + str(val0))
+
+    for key, val in features_dict.iteritems():
+        feat_str.write(' ' + key + '=' + str(val))
+
+    return feat_str.getvalue()
 
 
 if __name__ == '__main__':
@@ -50,8 +113,12 @@ if __name__ == '__main__':
     words_counter = get_words_counter(lines)
     stream = StringIO()
     for line in lines:
-        for word, tag in line:
-            # TODO: complete
+        features = {}
+        for i, (word, tag) in enumerate(line):
+            fill_with_regular_features(features, line, i)
+            fill_with_rareness_features(features, words_counter, word)
+            stream.write(tag + ' ' + get_features_str(features) + '\n')
+
     # write output
     output_file = open(output_filename, 'w')
     output_file.write(stream.getvalue())
