9b6b79275550ba9952d2c08755687eb00e5e47a5
Tamir Moshiashvili <norblet82@gmail.com>
2017-11-23 23:22:00 +0200

    # used ner_eval for memm1+2

--- memm1/TrainSolver.sh
+++ memm1/TrainSolver.sh
@@ -2,5 +2,5 @@
 # build model from features vectors file
 # input should be: feature_vecs_file output_model_file_name
 
-java -cp liblinear.jar de.bwaldvogel.liblinear.Train -s 0 $1 $2
+java -cp liblinear-1.94.jar de.bwaldvogel.liblinear.Train -s 0 $1 $2
 echo "Done!"
